5a104b0090fcc9c3f22425721642ccfc54085cb2
aswanth0123 <aswanthsuresh74@gmail.com>
2025-03-28 22:20:09 +0530

    home page editing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,26 @@
-function displayNav() {
-    document.getElementById('threedots').style.display = 'none';
-    document.getElementById('twolines').style.display = "flex";
-}
+// function displayNav() {
+//     document.getElementById('threedots').style.display = 'none';
+//     document.getElementById('twolines').style.display = "flex";
+// }
 
-function removeNav() {
-    document.getElementById('threedots').style.display = 'block';
-    document.getElementById('twolines').style.display = "none";
-}
+// function removeNav() {
+//     document.getElementById('threedots').style.display = 'block';
+//     document.getElementById('twolines').style.display = "none";
+// }
 
-const scroller = document.querySelector('.scroller');
-let scrollAmount = 0;
+// const scroller = document.querySelector('.scroller');
+// let scrollAmount = 0;
 
-function scrollImages() {
-    scrollAmount -= 1; // Adjust speed
-    if (scrollAmount <= -scroller.offsetWidth / 2) {
-        scrollAmount = 0; // Reset to create infinite loop
-    }
-    scroller.style.transform = `translateX(${scrollAmount}px)`;
-    requestAnimationFrame(scrollImages);
-}
+// function scrollImages() {
+//     scrollAmount -= 1; // Adjust speed
+//     if (scrollAmount <= -scroller.offsetWidth / 2) {
+//         scrollAmount = 0; // Reset to create infinite loop
+//     }
+//     scroller.style.transform = `translateX(${scrollAmount}px)`;
+//     requestAnimationFrame(scrollImages);
+// }
 
-scrollImages();
+// scrollImages();
 
 let add = document.querySelector(".add_opt");
 
