3c5d4cf30ab7e12736489cf22ccf6ccb0bc45272
aswanth0123 <aswanthsuresh74@gmail.com>
2025-03-29 17:59:43 +0530

    some editing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,13 @@
-// function displayNav() {
-//     document.getElementById('threedots').style.display = 'none';
-//     document.getElementById('twolines').style.display = "flex";
-// }
+function displayNav() {
+    document.getElementById('icon_div').style.display = 'none';
+    document.getElementById('twolines').style.display = "flex";
+}
 
-// function removeNav() {
-//     document.getElementById('threedots').style.display = 'block';
-//     document.getElementById('twolines').style.display = "none";
-// }
-
+function removeNav() {
+    document.getElementById('icon_div').style.display = 'block';
+    document.getElementById('twolines').style.display = "none";
+}
+// displayNav()
 // const scroller = document.querySelector('.scroller');
 // let scrollAmount = 0;
 
@@ -27,17 +27,17 @@ let add = document.querySelector(".add_opt");
 // Initially hide the div
 add.style.display = "none";
 
-setTimeout(() => {
-    document.querySelector(".main").style.opacity = 0.2;
+// setTimeout(() => {
+//     document.querySelector(".main").style.opacity = 0.2;
     
-    add.style.display = "flex"; 
-    add.style.opacity = "0";
-    add.style.transform = "scale(0.8)"; 
-    setTimeout(() => {
-        add.style.opacity = "1"; 
-        add.style.transform = "scale(1)"; 
-    }, 50);
-}, 1000);
+//     add.style.display = "flex"; 
+//     add.style.opacity = "0";
+//     add.style.transform = "scale(0.8)"; 
+//     setTimeout(() => {
+//         add.style.opacity = "1"; 
+//         add.style.transform = "scale(1)"; 
+//     }, 50);
+// }, 1000);
 
 function close_add() {
     let add = document.querySelector(".add_opt");
